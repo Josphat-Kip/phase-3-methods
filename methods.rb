ad7a29f5aa3f2e888e7a977e355bb8050ca9489f
Josphat-Kip <josphatkiplimo247@gmail.com>
2023-02-11 09:01:55 +0300

    2nd attempt

--- methods.rb
+++ methods.rb
@@ -1,25 +1,26 @@
-# Your code here!
 def greet_programmer
-    puts "Hello, programmer!"
+    puts "Hello, programmer!" 
 end
 
+greet_programmer
+def greet name
+    puts  "Hello, #{name}!"
+end
+greet ("Naureen")
 
-def greet(name)
+def greet_with_default name="programmer"
     puts "Hello, #{name}!"
 end
+greet_with_default ("Naureen")
 
-
-def greet_with_default(name = "programmer")
-    puts "Hello, #{name}!"
+def add num1 , num2
+    num1 + num2;
 end
 
+add(10,30)
 
-def add(num1, num2)
-    return num1 + num2
+def halve number 
+    return nil if number.class!=Integer
+    number/2
 end
-
-
-def halve(num)
-    return nil unless num.class == Integer
-    num / 2
-end
+halve(7)
